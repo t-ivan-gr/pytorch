ce101fae8b4da5176d547119f5475edc25b7e6ed
soulitzer <soulitzer@gmail.com>
2025-02-27 17:05:05 -0500

    torch.utils.checkpoint preserves torch function mode stack during recompute
    ghstack-source-id: c01200ad3cf6720f0bd8084022d413aa88491e63
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/148023

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -80,6 +80,7 @@ from torch.testing._internal.common_utils import (
     TestCase,
     xfailIfTorchDynamo,
 )
+from torch.overrides import TorchFunctionMode
 from torch.utils._mode_utils import no_dispatch
 from torch.utils._python_dispatch import TorchDispatchMode
 from torch.utils.checkpoint import (
@@ -7513,6 +7514,46 @@ for shape in [(1,), ()]:
             model_checkpoint_without_reentrant.parameters(),
         ):
             self.assertEqual(param.grad, checkpoint_param.grad)
+
+    @xfailIfTorchDynamo
+    def test_checkpointing_preserves_torch_function_mode_stack(self):
+        log = []
+
+        def get_mode_class(n):
+            class Func(TorchFunctionMode):
+                def __torch_function__(self, func, types, args, kwargs=None):
+                    kwargs = {} if kwargs is None else kwargs
+                    log.append(f"mode{n}")
+                    return func(*args, **kwargs)
+
+            return Func
+
+        Mode1 = get_mode_class(1)
+        Mode2 = get_mode_class(2)
+        Mode3 = get_mode_class(3)
+
+        def func(x):
+            return x.sin().cos()
+
+        def context_fn():
+            return Mode3(), Mode3()
+
+        with Mode1():
+            with Mode2():
+                a = torch.tensor(1.0, requires_grad=True)
+
+                log = []
+                out = checkpoint(func, a, use_reentrant=False, context_fn=context_fn)
+                self.assertTrue(
+                    log[-3] == "mode3" and log[-2] == "mode2" and log[-1] == "mode1"
+                )
+
+
+                log = []
+                out.backward()
+                self.assertTrue(
+                    log[-3] == "mode3" and log[-2] == "mode2" and log[1] == "mode1"
+                )
 
     def test_callback_adds_callback(self):
         called = [0]
