14bcdd0f8214f3a8ffa8a21ce33acb48dff28f40
Catherine Lee <csl@fb.com>
2025-02-28 09:18:42 -0800

    tc

--- .ci/docker/common/install_python.sh
+++ .ci/docker/common/install_python.sh
@@ -3,9 +3,9 @@ set -ex
 
 apt-get update
 apt-get install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python3-pip
-ln -s /usr/bin/python3.9 /usr/bin/python
+ln -s /usr/bin/python${PYTHON_VERSION} /usr/bin/python
 rm -rf /usr/bin/python3
-ln -s /usr/bin/python3.9 /usr/bin/python3
+ln -s /usr/bin/python${PYTHON_VERSION} /usr/bin/python3
 python -mpip install --upgrade pip
 python -mpip install -r /opt/requirements-ci.txt
 python -mpip install cmake
